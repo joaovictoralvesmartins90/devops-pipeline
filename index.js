a3f015aa3f4e3d73e02407d7bd4341b17ed1a0c0
João Víctor Alves Martins <jvam90@proton.me>
2026-09-21 22:42:20 -0300

    feat: add CI pipeline

---
+++ index.js
@@ -0,0 +1,2 @@
+console.log("Hello devops!");
+console.log("I'm learning devops");
---
+++ test.js
@@ -0,0 +1,5 @@
+console.log("Starting tests...");
+setTimeout(() => {
+  console.log("Running tests...");
+}, 3000);
+console.log("Tests completed");
